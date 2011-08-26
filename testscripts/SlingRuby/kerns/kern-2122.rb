89d04ad722148e80a327c7db8b0164196e5cbab1
John King <johnk@media.berkeley.edu>
2011-08-25 18:03:49 -0700

    KERN-2122 - fixed int test script to pass a title to create_full_group, otherwise group is not indexed which is why last asserts were failing

--- testscripts/SlingRuby/kerns/kern-2122.rb
+++ testscripts/SlingRuby/kerns/kern-2122.rb
@@ -34,18 +34,15 @@ class TC_Kern2122 < Test::Unit::TestCase
     @s.switch_user(viewer1)
     ticks = Time.now.to_f
     
-    
-    managed_group = @um.create_group "group-#{m}"
+    managed_group = @full_group_creator.create_full_group viewer1.name, "group-#{m}", "kern-2122 int test group", "kern-2122 int test group description"
     assert_not_nil(managed_group, "expected managed group to be created")
-    #add_manager_res = managed_group.add_manager @s, viewer1.name
-    #assert_equal("200",add_manager_res.code, "adding manager should have succeeded")
-      
+
     file_name = "random-#{ticks}.txt"
     file_body = "Add the time to make it sort of random #{Time.now.to_f}."
     upload_res = @fm.upload_pooled_file(file_name, file_body, 'text/plain')
     json = JSON.parse(upload_res.body)
     file_id = json[file_name]["poolId"]
-
+    
     wait_for_indexer()
     find_res = @s.execute_get(@s.url_for("/var/search/pool/me/manager-all.tidy.json"))
     assert_equal("200",find_res.code,find_res.body)
@@ -65,13 +62,16 @@ class TC_Kern2122 < Test::Unit::TestCase
     json = JSON.parse(members_res.body)
     assert_equal("200",members_res.code)
     viewers = json["viewers"]
-    assert_equal(3, viewers.length, "should be 2 viewers because viewer1 cannot remove viewer2" )
+    assert_equal(3, viewers.length, "should be 3 viewers because viewer1 cannot remove viewer2" )
     
     # now have viewer1 remove managed group
     # this is how to remove an item from a user's library
     remove_group_res = @fm.manage_members(file_id, nil, [managed_group.name], nil, nil)
     assert_equal("200",remove_group_res.code, "expected viewer1 to be able to remove a group managed by them")
-    
+    members_res = @fm.get_members file_id
+    assert_equal("200",members_res.code)    
+    json = JSON.parse(members_res.body)
+    viewers = json["viewers"]
     # now have viewer1 remove themself
     # this is how to remove an item from a user's library
     remove_viewer1_res = @fm.manage_members(file_id, nil, [viewer1.name], nil, nil)
@@ -83,9 +83,9 @@ class TC_Kern2122 < Test::Unit::TestCase
     assert_equal("200",members_res.code)    
     json = JSON.parse(members_res.body)
     viewers = json["viewers"]
-    # assert_equal(2, viewers.length, "should be 1 viewer left because viewer1 can remove themself from viewers" )
-    # remaining_viewer = viewers[0]
-    #assert_equal(viewer2.name, remaining_viewer["userid"], "should be viewer2 remaining after removal of viewer1" )
+    assert_equal(1, viewers.length, "should be 1 viewer left because viewer1 can remove themself from viewers" )
+    remaining_viewer = viewers[0]
+    assert_equal(viewer2.name, remaining_viewer["userid"], "should be viewer2 remaining after removal of viewer1" )
   end
     
 end
